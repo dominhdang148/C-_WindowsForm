95f3ab765b33c3809e01f89f11cc2088e8f96ab2
dominhdang148 <myclone235@gmail.com>
2022-06-12 17:17:42 +0700

    Bổ sung bài giữa kì

---
+++ BaiKT_GiuaKy/BaiKT_GiuaKy/FormCustomer.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace BaiKT_GiuaKy
+{
+    partial class FormCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.mtxtSDT = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDC = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.rbSoDT = new System.Windows.Forms.RadioButton();
+            this.rbTen = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvDanhSachKH = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachKH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã khách hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên khách hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số điện thoại";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(138, 20);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(241, 20);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(138, 52);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(241, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // mtxtSDT
+            // 
+            this.mtxtSDT.Location = new System.Drawing.Point(138, 84);
+            this.mtxtSDT.Mask = "0000000000";
+            this.mtxtSDT.Name = "mtxtSDT";
+            this.mtxtSDT.Size = new System.Drawing.Size(241, 20);
+            this.mtxtSDT.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(442, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Địa chỉ giao hàng";
+            // 
+            // txtDC
+            // 
+            this.txtDC.Location = new System.Drawing.Point(445, 52);
+            this.txtDC.Multiline = true;
+            this.txtDC.Name = "txtDC";
+            this.txtDC.Size = new System.Drawing.Size(290, 52);
+            this.txtDC.TabIndex = 7;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(267, 128);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Mặc định";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(445, 128);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.rbSoDT);
+            this.groupBox1.Controls.Add(this.rbTen);
+            this.groupBox1.Location = new System.Drawing.Point(349, 167);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 49);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tìm kiếm";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(191, 18);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(189, 20);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // rbSoDT
+            // 
+            this.rbSoDT.AutoSize = true;
+            this.rbSoDT.Location = new System.Drawing.Point(96, 19);
+            this.rbSoDT.Name = "rbSoDT";
+            this.rbSoDT.Size = new System.Drawing.Size(75, 17);
+            this.rbSoDT.TabIndex = 1;
+            this.rbSoDT.Text = "Theo SĐT";
+            this.rbSoDT.UseVisualStyleBackColor = true;
+            // 
+            // rbTen
+            // 
+            this.rbTen.AutoSize = true;
+            this.rbTen.Checked = true;
+            this.rbTen.Location = new System.Drawing.Point(15, 19);
+            this.rbTen.Name = "rbTen";
+            this.rbTen.Size = new System.Drawing.Size(68, 17);
+            this.rbTen.TabIndex = 0;
+            this.rbTen.TabStop = true;
+            this.rbTen.Text = "Theo tên";
+            this.rbTen.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Danh sách khách hàng";
+            // 
+            // dgvDanhSachKH
+            // 
+            this.dgvDanhSachKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDanhSachKH.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvDanhSachKH.Location = new System.Drawing.Point(32, 222);
+            this.dgvDanhSachKH.Name = "dgvDanhSachKH";
+            this.dgvDanhSachKH.Size = new System.Drawing.Size(702, 210);
+            this.dgvDanhSachKH.TabIndex = 12;
+            this.dgvDanhSachKH.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDanhSachKH_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "MaKH";
+            this.Column1.HeaderText = "Mã KH";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "TenKH";
+            this.Column2.FillWeight = 300F;
+            this.Column2.HeaderText = "Tên khách hàng";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 250;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "SoDienThoai";
+            this.Column3.HeaderText = "Số ĐT";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "DiaChiGH";
+            this.Column4.FillWeight = 200F;
+            this.Column4.HeaderText = "Địa chỉ giao hàng";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 200;
+            // 
+            // FormCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 450);
+            this.Controls.Add(this.dgvDanhSachKH);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtDC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mtxtSDT);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormCustomer";
+            this.Text = "Quản lý khách hàng";
+            this.Load += new System.EventHandler(this.FormCustomer_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachKH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.MaskedTextBox mtxtSDT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDC;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.RadioButton rbSoDT;
+        private System.Windows.Forms.RadioButton rbTen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvDanhSachKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ BaiKT_GiuaKy/BaiKT_GiuaKy/FormCustomer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiKT_GiuaKy
+{
+    public partial class FormCustomer : Form
+    {
+        private string connectionString = "";
+        public FormCustomer()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+        }
+
+        private void FormCustomer_Load(object sender, EventArgs e)
+        {
+            
+            HienThiDanhSachKhachHang();
+        }
+
+        private void HienThiDanhSachKhachHang()
+        {
+            var connection = new SqlConnection(connectionString);
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM KhachHang";
+
+            var adapter = new SqlDataAdapter(command);
+            var table = new DataTable("KhachHang");
+
+            connection.Open();
+            adapter.Fill(table);
+            connection.Close();
+
+            dgvDanhSachKH.DataSource = table;
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtID.Text = "";
+            txtName.Text = "";
+            mtxtSDT.Text = "";
+            txtDC.Text = "";
+        }
+
+        private void dgvDanhSachKH_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtID.Text = dgvDanhSachKH.SelectedRows[0].Cells[0].Value.ToString();
+            txtName.Text = dgvDanhSachKH.SelectedRows[0].Cells[1].Value.ToString();
+            mtxtSDT.Text = dgvDanhSachKH.SelectedRows[0].Cells[2].Value.ToString();
+            txtDC.Text = dgvDanhSachKH.SelectedRows[0].Cells[3].Value.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var id = txtID.Text;
+            var name = txtName.Text;
+            var sdt = mtxtSDT.Text;
+            var dc = txtDC.Text;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Lỗi! Xin hãy nhập đầy đủ thông tin cần thiết", "Thông báo");
+                return;
+            }
+
+            var connection = new SqlConnection(connectionString);
+            var command = connection.CreateCommand();
+
+            if (id == "")
+            {
+                //INSERT [dbo].[KhachHang] ([TenKH], [SoDienThoai], [DiaChiGH]) VALUES (N'Nga', N'0985785745 ', NULL)
+                command.CommandText = "INSERT [dbo].[KhachHang] ([TenKH], [SoDienThoai], [DiaChiGH]) VALUES (@Name,@SDT, @DC)";
+            }
+            else
+            {
+                command.CommandText = "UPDATE KhachHang set TenKH=@Name, SoDienThoai=@SDT, DiaChiGH=@DC WHERE MaKH=@id";
+            }
+
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@name", name);
+            if (sdt != null)
+            {
+                command.Parameters.AddWithValue("@SDT", sdt);
+            }
+            else
+            {
+                command.Parameters.AddWithValue("@SDT", "NULL");
+            }
+
+            if (dc != null)
+            {
+                command.Parameters.AddWithValue("@DC", dc);
+            }
+            else
+            {
+                command.Parameters.AddWithValue("@DC", "NULL");
+            }
+            connection.Open();
+            var numberOfLine = command.ExecuteNonQuery();
+            connection.Close();
+            if (numberOfLine > 0)
+            {
+                MessageBox.Show("Thao tác thành công!", "Thông báo");
+                btnReset.PerformClick();
+                HienThiDanhSachKhachHang();
+            }
+            else
+            {
+                MessageBox.Show("Thao tác thất bại", "Thông báo");
+            }
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            var search_result = txtSearch.Text;
+            var connection = new SqlConnection(connectionString);
+            var command = connection.CreateCommand();
+            if (rbTen.Checked)
+            {
+                command.CommandText = "select * from KhachHang where TenKH like N'%" + search_result + "%'";
+            }
+            else
+            {
+                command.CommandText= "select * from KhachHang where SoDienThoai like '%" + search_result + "%'";
+            }
+
+            var adapter = new SqlDataAdapter(command);
+            var table = new DataTable("KhachHang");
+
+            connection.Open();
+            adapter.Fill(table);
+            connection.Close();
+
+            dgvDanhSachKH.DataSource = table;
+        }
+    }
+}
